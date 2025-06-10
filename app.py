f12dc40fe2d7457282839105f097cbe673f47bb5
kchauu6ff <kchauuff@gmail.com>
2025-06-10 19:03:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request
-from pyexpat.errors import messages
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get('name', 'Recruto')
-    message = request.args.get ('message', 'Давай дружить')
+    message = request.args.get('message', 'Давай дружить')
     return f"Hello {name}! {message}!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
